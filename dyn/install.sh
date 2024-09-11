8e9381b054c02d114d0efecbfe7bd1158d490cf1
Restart <45609733+RestartFU@users.noreply.github.com>
2024-09-11 01:48:10 -0400

    Update By Trusted Contributor

--- dyn/install.sh
+++
@@ -1,5 +0,0 @@
-redirect=$(curl -w "%{redirect_url}" -o /dev/null -s "https://github.com/RestartFU/dyn/releases/latest/download/dyn")
-download_url=$(curl -w "%{redirect_url}" -o /dev/null -s $redirect)
-curl -o dyn $download_url
-chmod +x dyn
-sudo mv dyn /usr/bin/dyn
