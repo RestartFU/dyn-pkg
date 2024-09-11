68bc2ee1502a2ba05f860729d3ed101308e408d6
Restart <45609733+RestartFU@users.noreply.github.com>
2024-09-11 00:27:54 -0400

    Update By Trusted Contributor

--- discord/install.sh
+++ discord/install.sh
@@ -12,8 +12,7 @@ killall Discord > /dev/null 2>&1 &
 rm -rf ~/.discord
 sudo rm -rf /usr/bin/discord
 
-discord_path=$(realpath ~/.discord/Discord)
-
 mv Discord ~/.discord
+discord_path=$(realpath ~/.discord/Discord)
 sudo ln -s $discord_path /usr/bin/discord
 echo "symlink created for $discord_path to /usr/bin/discord"
